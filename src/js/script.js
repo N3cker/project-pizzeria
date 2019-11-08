9c5c1273dcd92c229664212dd3c2c3dd21a3056b
Mateusz Piasecki <piaszuu@gmail.com>
2019-11-08 17:22:58 +0100

    init Accordin

--- src/js/script.js
+++ src/js/script.js
@@ -55,10 +55,32 @@
   class Product{
     constructor(id, data){
       const thisProduct = this;
+
       thisProduct.id = id;
-      thisProduct.data = data;
+      thisProduct.data = data;  
+    
+      thisProduct.renderInMenu();
+      thisProduct.initAccordin();
       console.log('new Product:', thisProduct);
     }
+
+    renderInMenu(){
+    const thisProduct = this;
+
+    /* generTE HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+    /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+    /* find menu conteiner */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+    /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordin(){
+      const thisProcuct = this;
+    }
+
   }
   const app = {
     initMenu: function(){
